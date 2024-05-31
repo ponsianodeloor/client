c6febd2f478a94bd79249edc3b93acb0ed47f51d
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-05-30 23:01:19 -0500

    pruebas unitarias

--- src/test/java/com/sofka/client/ClientApplicationTests.java
+++ src/test/java/com/sofka/client/ClientApplicationTests.java
@@ -1,13 +1,41 @@
 package com.sofka.client;
 
+import com.sofka.client.model.Client;
+import com.sofka.client.repository.ClientRepository;
+import com.sofka.client.service.ClientService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ClientApplicationTests {
 
+
+	@Autowired
+	private ClientService clientService;
+
+	@MockBean
+	private ClientRepository clientRepository;
+
 	@Test
-	void contextLoads() {
+	void testGetAllClients() {
+		Client client1 = new Client();
+		client1.setUsername("jlema");
+		Client client2 = new Client();
+		client2.setUsername("mmontalvo");
+
+		List<Client> expectedClients = Arrays.asList(client1, client2);
+		when(clientRepository.findAll()).thenReturn(expectedClients);
+
+		List<Client> actualClients = clientService.getAllClients();
+		assertEquals(expectedClients, actualClients);
 	}
 
 }
